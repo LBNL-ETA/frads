68de0bdce781b85dacf7c52e5060938ac288f625
Taoning Wang <taoningw@gmail.com>
2021-04-21 15:18:38 -0700

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
+              'sphinxcontrib.programoutput',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
