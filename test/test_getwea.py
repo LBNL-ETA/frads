867ba625a727ec2a10abbd216a5f3dafedfb69d6
Taoning Wang <taoningw@gmail.com>
2022-03-15 11:19:34 -0700

    test: improve test for getwea

--- test/test_getwea.py
+++ test/test_getwea.py
@@ -1,24 +1,46 @@
+"""
+Unittests for getwea
+"""
+
 import unittest
 import subprocess as sp
-import os
-import shutil
-from frads import makesky, radutil
+
 
 class TestGetwea(unittest.TestCase):
+    """Test functionality associated with getwea command-line tool."""
 
     def test_help(self):
+        """Test starting getwea."""
         cmd = ["getwea", "-h"]
         process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
-        self.assertEqual(process.stderr, b'')
+        self.assertEqual(process.stderr, b"")
+
+    def test_us_latlon(self):
+        """Test downloading US EPW from Lat&Lon."""
+        cmd = ["getwea", "-a", "37", "-o", "122"]
+        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
+        self.assertEqual(process.stderr, b"")
+
+    def test_non_us_latlon(self):
+        """Test downloading non-US EPW from Lat&Lon."""
+        cmd = ["getwea", "-a", "28", "-o", "-122"]
+        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
+        self.assertEqual(process.stderr, b"")
+
+    def test_us_zip(self):
+        """Test downloading US EPW using zipcode."""
+        cmd = ["getwea", "-z", "94720"]
+        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
+        self.assertEqual(process.stderr, b"")
 
     def test_sun_mtx(self):
+        """Test download and generating .wea based on window orientation
+        and remove entries with zero solar radiance.
+        """
         window_path = "./Objects/upper_glass.rad"
-        cmd = ['getwea', '-a', '37', '-o', '122', '-wpths', window_path, '-rz']
-        process = sp.run(cmd, stderr=sp.PIPE, stdout=sp.PIPE)
-        print(process.stderr)
-        self.assertEqual(process.stderr, b'')
-        with open('test.wea', 'wb') as wtr:
-            wtr.write(process.stdout)
+        cmd = ["getwea", "-a", "37", "-o", "122", "-wpths", window_path, "-rz"]
+        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
+        self.assertEqual(process.stderr, b"")
 
 
 if __name__ == "__main__":
