67c91c66639da489f12d2c775ed45fda03707179
Taoning Wang <taoning@gmail.com>
2022-10-26 22:36:29 -0700

    test:force manual testings for mrad and genmtx

--- test/_test_genmtx.py
+++ test/_test_genmtx.py
@@ -1,100 +1,24 @@
-import unittest
+from pathlib import Path
 import subprocess as sp
 import os
 import glob
 import shutil
-from frads import makesky, radutil, util
 
 
-class TestGenmtx(unittest.TestCase):
-    def test_help(self):
-        cmd = ["genmtx", "-h"]
-        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
-        self.assertEqual(process.stderr, b"")
+def test_help():
+    cmd = ["gen", "-h"]
+    sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
 
-    def test_sun_mtx(self):
-        window_path = os.path.join("Objects", "upper_glass.rad")
-        window_normals = radutil.primitive_normal([window_path])
-        with open("Resources/USA_CA_Oakland.Intl.AP.724930_TMY3.epw") as rdr:
-            epw = rdr.read()
-        wea_metadata, wea_data = makesky.epw2wea(
-            epw,
-            dhour=True,
-            shour=6,
-            ehour=20,
-            remove_zero=True,
-            window_normal=window_normals,
-        )
-        smx = makesky.gendaymtx(wea_data, wea_metadata, mf=6, direct=True, onesun=True)
-        with open("test.smx", "wb") as wtr:
-            wtr.write(smx)
-        cmd = [
-            "genmtx",
-            "-st",
-            "v",
-            "-s",
-            "v1a.vf",
-            "-r",
-            "sun",
-            "-rs",
-            "r6",
-            "-o",
-            "test_genmtx_sun_mtx",
-            "-wpths",
-            window_path,
-            "-opt",
-            "-ab 0",
-            "-res",
-            "8",
-            "8",
-            "-smx",
-            "test.smx",
-        ]
-        process = sp.run(cmd, stderr=sp.PIPE)
-        print(process.stderr)
-        self.assertEqual(process.stderr, b"")
-        self.assertTrue(os.path.isdir("test_genmtx_sun_mtx"))
-        self.assertEqual(len(glob.glob("test_genmtx_sun_mtx/*.hdr")), 542)
-        shutil.rmtree("test_genmtx_sun_mtx")
-        os.remove("test.smx")
+def test_matrix_point_sky():
+    grid_path = Path("test", "Resources", "grid.pts")
+    sys_paths = [
+        Path("test", "Objects/materials.mat"),
+        Path("test", "Objects/walls.rad"),
+        Path("test", "Objects/ceiling.rad"),
+        Path("test", "Objects/floor.rad"),
+    ]
+    cmd = ["gen", "matrix", "point-sky", grid_path, *sys_paths]
+    sp.run(cmd, check=True)
+    assert os.path.isfile("grid_r4sky.mtx")
+    os.remove("grid_r4sky.mtx")
 
-    def test_sun_mtx2(self):
-        with open("Resources/USA_CA_Oakland.Intl.AP.724930_TMY3.epw") as rdr:
-            epw = rdr.read()
-        wea_metadata, wea_data = makesky.epw2wea(
-            epw, dhour=True, shour=6, ehour=20, remove_zero=True
-        )
-        smx = makesky.gendaymtx(wea_data, wea_metadata, mf=6, direct=True, onesun=True)
-        with open("test.smx", "wb") as wtr:
-            wtr.write(smx)
-        cmd = [
-            "genmtx",
-            "-st",
-            "v",
-            "-s",
-            "v1a.vf",
-            "-r",
-            "sun",
-            "-rs",
-            "r6",
-            "-o",
-            "test_genmtx_sun_mtx2",
-            "-opt",
-            "-ab 0",
-            "-res",
-            "8",
-            "8",
-            "-smx",
-            "test.smx",
-        ]
-        process = sp.run(cmd, stderr=sp.PIPE)
-        print(process.stderr)
-        self.assertEqual(process.stderr, b"")
-        self.assertTrue(os.path.isdir("test_genmtx_sun_mtx2"))
-        self.assertEqual(len(glob.glob("test_genmtx_sun_mtx2/*.hdr")), 615)
-        shutil.rmtree("test_genmtx_sun_mtx2")
-        os.remove("test.smx")
-
-
-if __name__ == "__main__":
-    unittest.main()
--- test/_test_mrad.py
+++ test/_test_mrad.py
@@ -1,73 +1,86 @@
-import unittest
-import subprocess as sp
+import argparse
 import os
-import glob
+from pathlib import Path
+import sys
+sys.path.append('.')
 import shutil
 
-
-class TestMrad(unittest.TestCase):
-    def test_0_help(self):
-        cmd = ["mrad", "-h"]
-        process = sp.run(cmd, check=True, stderr=sp.PIPE, stdout=sp.PIPE)
-        self.assertEqual(process.stderr, b"")
-
-    def test_1_init(self):
-        cmd = ["mrad", "init", "-n", "test", "-W", "Resources/test.wea"]
-        process = sp.run(cmd, check=True, stderr=sp.PIPE)
-        self.assertTrue(os.path.isfile("test.cfg"))
-        os.remove("test.cfg")
-
-    def test_1_init2(self):
-        cmd = [
-            "mrad",
-            "init",
-            "--epw_path",
-            "Resources/USA_CA_Oakland.Intl.AP.724930_TMY3.epw",
-        ]
-        process = sp.run(cmd, check=True, stderr=sp.PIPE)
-        self.assertTrue(os.path.isfile("default.cfg"))
-        os.remove("default.cfg")
-
-    def test_2_two_phase(self):
-        cmd = ["mrad", "-vvvv", "run", "two_phase.cfg"]
-        proc = sp.run(cmd, check=True, stderr=sp.PIPE)
-        self.assertEqual(proc.stderr, b"")
-        self.assertTrue(os.path.isfile("./Matrices/pdsmx_floor.mtx"))
-        dsmx_size = os.stat("./Matrices/pdsmx_floor.mtx").st_size
-        self.assertGreaterEqual(dsmx_size, 2.5e6)
-        self.assertTrue(os.path.isdir("./Matrices/vdsmx_view_00"))
-        vsmx_size = len(os.listdir("./Matrices/vdsmx_view_00"))
-        self.assertEqual(vsmx_size, 146)
-        view_results = glob.glob("Results/view_two_phase_view_00/*.hdr")
-        self.assertEqual(len(view_results), 2)
-        shutil.rmtree("Matrices")
-        shutil.rmtree("Results")
-
-    def test_3_three_phase(self):
-        cmd = ["mrad", "-vvvv", "run", "three_phase.cfg"]
-        proc = sp.run(cmd, check=True, stderr=sp.PIPE)
-        self.assertEqual(proc.stderr, b"")
-        view_results = glob.glob("Results/view_three_phase_view_00/*.hdr")
-        self.assertEqual(len(view_results), 1)
-        shutil.rmtree("Matrices")
-        shutil.rmtree("Results")
-
-    def test_4_five_phase(self):
-        cmd = ["mrad", "-v", "run", "five_phase.cfg"]
-        proc = sp.run(cmd, check=True, stderr=sp.PIPE)
-        view_results = glob.glob("Results/view_five_phase_view_00/*.hdr")
-        self.assertEqual(len(view_results), 4387)
-        shutil.rmtree("Matrices")
-        shutil.rmtree("Results")
-
-    def test_5_five_phase2(self):
-        cmd = ["mrad", "-v", "run", "five_phase2.cfg"]
-        proc = sp.run(cmd, check=True, stderr=sp.PIPE)
-        view_results = glob.glob("Results/view_five_phase2_view_00/*.hdr")
-        self.assertEqual(len(view_results), 4387)
-        shutil.rmtree("Matrices")
-        shutil.rmtree("Results")
+from frads import cli
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_init():
+    os.chdir("test")
+    args = argparse.Namespace()
+    args.name = "test"
+    args.wea_path = Path("Resources/test.wea")
+    args.object = [Path("Objects/wall.rad")]
+    args.material = [Path("Objects/materials.mat")]
+    args.grid = None
+    args.bsdf = None
+    args.window = None
+    cli.mrad_init(args)
+    assert os.path.isfile("test.cfg")
+    os.remove("test.cfg")
+    args.wea_path = None
+    args.name = "default"
+    args.epw_path = Path("Resources/USA_CA_Oakland.Intl.AP.724930_TMY3.epw")
+    cli.mrad_init(args)
+    assert os.path.isfile("default.cfg")
+    os.remove("default.cfg")
+    os.chdir("..")
+
+
+def test_two_phase():
+    os.chdir("test")
+    args = argparse.Namespace()
+    args.verbose = 4
+    args.cfg = Path("two_phase.cfg")
+    cli.mrad_run(args)
+    assert os.path.isfile("Matrices/pdsmx_two_phase_floor.mtx")
+    dsmx_size = os.stat("Matrices/pdsmx_two_phase_floor.mtx").st_size
+    assert dsmx_size >= 2.5e6
+    assert os.path.isdir("Matrices/vdsmx_two_phase_view_00")
+    vsmx_size = len(os.listdir("Matrices/vdsmx_two_phase_view_00"))
+    assert vsmx_size == 2306
+    view_results = Path("Results/view_two_phase_view_00").glob("*.hdr")
+    assert len(list(view_results)) == 2
+    shutil.rmtree("Matrices")
+    shutil.rmtree("Results")
+    os.chdir("..")
+
+
+def test_three_phase():
+    os.chdir("test")
+    args = argparse.Namespace()
+    args.verbose = 4
+    args.cfg = Path("three_phase.cfg")
+    cli.mrad_run(args)
+    view_results = Path("Results/view_three_phase_view_00").glob("*.hdr")
+    assert len(list(view_results)) == 1
+    shutil.rmtree("Matrices")
+    shutil.rmtree("Results")
+    os.chdir("..")
+
+def test_five_phase():
+    os.chdir("test")
+    args = argparse.Namespace()
+    args.verbose = 4
+    args.cfg = Path("five_phase.cfg")
+    cli.mrad_run(args)
+    view_results = Path("Results/view_five_phase_view_00").glob("*.hdr")
+    assert len(list(view_results)) == 4387
+    shutil.rmtree("Matrices")
+    shutil.rmtree("Results")
+    os.chdir("..")
+
+def test_five_phase2():
+    os.chdir("test")
+    args = argparse.Namespace()
+    args.verbose = 4
+    args.cfg = Path("five_phase2.cfg")
+    cli.mrad_run(args)
+    view_results = Path("Results/view_five_phase2_view_00").glob("*.hdr")
+    assert len(list(view_results)) == 4387
+    shutil.rmtree("Matrices")
+    shutil.rmtree("Results")
+    os.chdir("..")
