8cba03bc472ce67fec8c92f8c18210d2146c63db
Taoning Wang <taoning@gmail.com>
2022-10-26 22:28:48 -0700

    refactor(matrix):light refactoring

--- frads/matrix.py
+++ frads/matrix.py
@@ -9,23 +9,27 @@ import os
 from pathlib import Path
 import subprocess as sp
 import tempfile as tf
+from typing import Iterable
+from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Union
 
 from frads import sky
 from frads import geom
-from frads import utils
 from frads import parsers
+from frads import raycall
+from frads import utils
 from frads.types import Primitive
 from frads.types import Receiver
 from frads.types import Sender
+from frads.geom import Vector
 
 
-logger = logging.getLogger("frads.matrix")
+logger: logging.Logger = logging.getLogger("frads.matrix")
 
 
-def surface_as_sender(prim_list: list, basis: str, offset=None, left=None):
+def surface_as_sender(prim_list: list, basis: str, offset=None, left=None) -> Sender:
     """
     Construct a sender from a surface.
 
@@ -42,15 +46,16 @@ def surface_as_sender(prim_list: list, basis: str, offset=None, left=None):
     prim_str = prepare_surface(
         prims=prim_list, basis=basis, offset=offset, left=left, source=None, out=None
     )
+    logger.debug("Surface sender:\n%s", prim_str)
     return Sender("s", prim_str.encode(), None, None)
 
 
-def view_as_sender(vu_dict: dict, ray_cnt: int, xres: int, yres: int) -> Sender:
+def view_as_sender(view: View, ray_cnt: int, xres: int, yres: int) -> Sender:
     """
     Construct a sender from a view.
 
     Args:
-        vu_dict: a dictionary containing view parameters;
+        view: a view object;
         ray_cnt: ray count;
         xres, yres: image resolution
         c2c: Set to True to trim the fisheye corner rays.
@@ -63,13 +68,14 @@ def view_as_sender(vu_dict: dict, ray_cnt: int, xres: int, yres: int) -> Sender:
         raise ValueError("Need to specify resolution")
     res_cmd = [
         "vwrays",
-        *(utils.opt2list(vu_dict)),
+        *view.args(),
         "-x",
         str(xres),
         "-y",
         str(yres),
         "-d",
     ]
+    logger.info("Check real image resolution: \n%s", " ".join(res_cmd))
     res_proc = sp.run(res_cmd, check=True, stdout=sp.PIPE, encoding="ascii")
     res_eval = res_proc.stdout.split()
     new_xres, new_yres = int(res_eval[1]), int(res_eval[3])
@@ -77,17 +83,21 @@ def view_as_sender(vu_dict: dict, ray_cnt: int, xres: int, yres: int) -> Sender:
         logger.info("Changed resolution to %s %s", new_xres, new_yres)
     vwrays_cmd = ["vwrays", "-ff", "-x", str(new_xres), "-y", str(new_yres)]
     if ray_cnt > 1:
-        vu_dict["c"] = ray_cnt
-        vu_dict["pj"] = 0.7  # placeholder
+        vwrays_cmd.extend(["-c", str(ray_cnt), "-pj", "0.7"])
     logger.debug("Ray count is %s", ray_cnt)
-    vwrays_cmd += utils.opt2list(vu_dict)
+    vwrays_cmd += view.args()
+    logger.info("Generate view rays with: \n%s", " ".join(vwrays_cmd))
     vwrays_proc = sp.run(vwrays_cmd, check=True, stdout=sp.PIPE)
-    if vu_dict["vt"] == "a":
-        flush_cmd = utils.flush_corner_rays_cmd(ray_cnt, xres)
-        flush_proc = sp.run(flush_cmd, input=vwrays_proc.stdout, stdout=sp.PIPE)
+    if view.vtype == "a":
+        flush_cmd = utils.get_flush_corner_rays_command(ray_cnt, xres)
+        logger.info("Flushing -vta corner rays: \n%s", " ".join(flush_cmd))
+        flush_proc = sp.run(
+            flush_cmd, check=True, input=vwrays_proc.stdout, stdout=sp.PIPE
+        )
         vrays = flush_proc.stdout
     else:
         vrays = vwrays_proc.stdout
+    logger.debug("View sender:\n%s", vrays)
     return Sender("v", vrays, xres, yres)
 
 
@@ -109,10 +119,16 @@ def points_as_sender(pts_list: list, ray_cnt: Optional[int] = None) -> Sender:
         raise ValueError("All grid points has to be lists.")
     pts_list = [i for i in pts_list for _ in range(ray_cnt)]
     grid_str = os.linesep.join([" ".join(map(str, li)) for li in pts_list]) + os.linesep
+    logger.debug("Point sender:\n%s", grid_str)
     return Sender("p", grid_str.encode(), None, len(pts_list))
 
 
-def sun_as_receiver(basis, smx_path, window_normals, full_mod=False) -> Receiver:
+def sun_as_receiver(
+    basis,
+    smx_path: Path,
+    window_normals: Optional[List[Vector]],
+    full_mod: bool = False,
+) -> Receiver:
     """
     Instantiate a sun receiver object.
 
@@ -129,13 +145,16 @@ def sun_as_receiver(basis, smx_path, window_normals, full_mod=False) -> Receiver
         str_repr, mod_str = sky.gen_sun_source_full(int(basis[-1]))
         return Receiver(str_repr, basis, modifier=mod_str)
     str_repr, mod_str, mod_str_full = sky.gen_sun_source_culled(
-        int(basis[-1]), smx_path=smx_path, window_normals=window_normals)
+        int(basis[-1]), smx_path=smx_path, window_normals=window_normals
+    )
     if full_mod:
         return Receiver(receiver=str_repr, basis=basis, modifier=mod_str_full)
+    logger.debug("Sun receiver:\n%s", str_repr)
+    logger.debug("Sun modifier:\n%s", mod_str)
     return Receiver(receiver=str_repr, basis=basis, modifier=mod_str)
 
 
-def sky_as_receiver(basis) -> Receiver:
+def sky_as_receiver(basis: str, out) -> Receiver:
     """
     Instantiate a sky receiver object.
 
@@ -147,8 +166,10 @@ def sky_as_receiver(basis) -> Receiver:
 
     if not basis.startswith("r"):
         raise ValueError(f"Sky basis need to be Treganza/Reinhart, found {basis}")
-    sky_str = sky.basis_glow(basis)
-    logger.debug(sky_str)
+    out.parent.mkdir(exist_ok=True)
+    sky_str = f'#@rfluxmtx o="{str(out)}"\n'
+    sky_str += sky.basis_glow(basis)
+    logger.debug("Sky receiver:\n%s", sky_str)
     return Receiver(sky_str, basis)
 
 
@@ -157,8 +178,8 @@ def surface_as_receiver(
     basis: str,
     out: Union[None, str, Path],
     offset=None,
-    left=False,
-    source="glow",
+    left: bool = False,
+    source: str = "glow",
 ) -> Receiver:
     """
     Instantiate a surface receiver object.
@@ -176,6 +197,7 @@ def surface_as_receiver(
     rcvr_str = prepare_surface(
         prims=prim_list, basis=basis, offset=offset, left=left, source=source, out=out
     )
+    logger.debug("Surface receiver:\n%s", rcvr_str)
     return Receiver(rcvr_str, basis)
 
 
@@ -196,19 +218,24 @@ def prepare_surface(*, prims, basis, left, offset, source, out) -> str:
 
     if basis is None:
         raise ValueError("Sampling basis cannot be None")
-    upvector = str(utils.up_vector(prims)).replace(" ", ",")
-    upvector = "-" + upvector if left else upvector
+    if (source is not None) and (source not in ("glow", "light")):
+        raise ValueError(f"Unknown source type {source}")
+    upvector = utils.up_vector(prims)
+    upvector = upvector.scale(-1) if left else upvector
+    upvector_str = str(upvector).replace(" ", ",")
     modifier_set = {p.modifier for p in prims}
     if len(modifier_set) != 1:
         logger.warning("Primitives don't share modifier")
-    src_mod = f"rflx{prims[0].modifier}"
-    src_mod += utils.id_generator()
-    header = f"#@rfluxmtx h={basis} u={upvector}\n"
+    src_mod = f"rflx{prims[0].modifier}{utils.id_generator()}"
+    header = f"#@rfluxmtx h={basis} u={upvector_str}\n"
     if out is not None:
         header += f'#@rfluxmtx o="{out}"\n\n'
-    if source is not None:
-        source_line = f"void {source} {src_mod}\n0\n0\n4 1 1 1 0\n\n"
-        header += source_line
+    if source == "glow":
+        source_prim = Primitive("void", source, src_mod, ("0"), (4, 1, 1, 1, 0))
+        header += str(source_prim)
+    elif source == "light":
+        source_prim = Primitive("void", source, src_mod, ("0"), (3, 1, 1, 1))
+        header += str(source_prim)
     modifiers = [p.modifier for p in prims]
     content = ""
     for prim in prims:
@@ -219,7 +246,7 @@ def prepare_surface(*, prims, basis, left, offset, source, out) -> str:
         _modifier = src_mod
         if offset is not None:
             poly = parsers.parse_polygon(prim.real_arg)
-            offset_vec = poly.normal().scale(offset)
+            offset_vec = poly.normal.scale(offset)
             moved_pts = [pt + offset_vec for pt in poly.vertices]
             _real_args = geom.Polygon(moved_pts).to_real()
         else:
@@ -231,7 +258,12 @@ def prepare_surface(*, prims, basis, left, offset, source, out) -> str:
     return header + content
 
 
-def rfluxmtx(*, sender, receiver, env, opt=None, out=None):
+def rfluxmtx(
+    sender: Sender,
+    receiver: Receiver,
+    env: Iterable[Path],
+    opt: Optional[List[str]] = None,
+) -> None:
     """
     Calling rfluxmtx to generate the matrices.
 
@@ -241,49 +273,39 @@ def rfluxmtx(*, sender, receiver, env, opt=None, out=None):
         env: model environment, basically anything that's not the
             sender or receiver
         opt: option string
-        out: output path
 
     Returns:
         return the stdout of the rfluxmtx run.
     """
     if None in (sender, receiver):
         raise ValueError("Sender/Receiver object is None")
-    opt = "" if opt is None else opt
+    opt = [] if opt is None else opt
+    _sender = None
+    stdin: Optional[bytes] = sender.sender
     with tf.TemporaryDirectory() as tempd:
-        receiver_path = os.path.join(tempd, "receiver")
-        with open(receiver_path, "w") as wtr:
+        receiver_path = Path(tempd, "receiver")
+        with open(receiver_path, "w", encoding="ascii") as wtr:
             wtr.write(receiver.receiver)
-        if isinstance(env[0], dict):
-            env_path = os.path.join(tempd, "env")
-            with open(env_path, "w") as wtr:
-                [wtr.write(str(prim)) for prim in env]
-            env_paths = [env_path]
-        else:
-            env_paths = env
-        cmd = ["rfluxmtx"] + opt.split()
-        stdin = None
         if sender.form == "s":
-            sender_path = os.path.join(tempd, "sender")
+            sender_path = Path(tempd, "sender")
             with open(sender_path, "wb") as wtr:
                 wtr.write(sender.sender)
-            cmd.extend([sender_path, receiver_path])
+            _sender = sender_path
+            stdin = None
         elif sender.form == "p":
-            cmd.extend(["-I+", "-faa", "-y", str(sender.yres), "-", receiver_path])
-            stdin = sender.sender
+            opt.extend(["-I+", "-faa", "-y", str(sender.yres)])
         elif sender.form == "v":
-            cmd.extend(["-ffc", "-x", str(sender.xres), "-y", str(sender.yres), "-ld-"])
-            if out is not None:
-                out = Path(out)
-                out.mkdir(exist_ok=True)
-                out = out / "%04d.hdr"
-                cmd.extend(["-o", str(out)])
-            cmd.extend(["-", receiver_path])
-            stdin = sender.sender
-        cmd.extend(env_paths)
-        return utils.spcheckout(cmd, inp=stdin)
+            opt.extend(["-ffc", "-x", str(sender.xres), "-y", str(sender.yres), "-ld-"])
+        cmd = raycall.get_rfluxmtx_command(
+            receiver_path, option=opt, sender=_sender, sys_paths=env
+        )
+        logger.info("Running rfluxmtx with:\n%s", " ".join(cmd))
+        proc = sp.run(cmd, check=True, input=stdin, stderr=sp.PIPE)
+        if proc.stderr != b"":
+            logger.warning(proc.stderr.decode())
 
 
-def rcvr_oct(receiver, env, oct_path: Union[str, Path]):
+def rcvr_oct(receiver, env, oct_path: Union[str, Path]) -> None:
     """
     Generate an octree of the environment and the receiver.
 
@@ -297,15 +319,21 @@ def rcvr_oct(receiver, env, oct_path: Union[str, Path]):
 
     with tf.TemporaryDirectory() as tempd:
         receiver_path = os.path.join(tempd, "rcvr_path")
-        with open(receiver_path, "w") as wtr:
+        with open(receiver_path, "w", encoding="utf-8") as wtr:
             wtr.write(receiver.receiver)
-        ocmd = ["oconv", "-f"] + env + [receiver_path]
-        octree = utils.spcheckout(ocmd)
+        ocmd = ["oconv", "-f", *map(str, env), receiver_path]
+        logger.info("Generate octree with:\n%s", " ".join(ocmd))
         with open(oct_path, "wb") as wtr:
-            wtr.write(octree)
+            sp.run(ocmd, check=True, stdout=wtr)
 
 
-def rcontrib(*, sender, modifier: str, octree: Union[str, Path], out, opt) -> None:
+def rcontrib(
+    sender,
+    modifier: str,
+    octree: Union[str, Path],
+    out: Union[str, Path],
+    opt: List[str],
+) -> None:
     """
     Calling rcontrib to generate the matrices.
 
@@ -320,13 +348,12 @@ def rcontrib(*, sender, modifier: str, octree: Union[str, Path], out, opt) -> No
         None
 
     """
-    lopt = opt.split()
-    lopt.append("-fo+")
+    opt.append("-fo+")
     with tf.TemporaryDirectory() as tempd:
         modifier_path = os.path.join(tempd, "modifier")
-        with open(modifier_path, "w") as wtr:
+        with open(modifier_path, "w", encoding="utf-8") as wtr:
             wtr.write(modifier)
-        cmd = ["rcontrib"] + lopt
+        cmd = ["rcontrib", *opt]
         stdin = sender.sender
         if sender.form == "p":
             cmd += ["-I+", "-faf", "-y", str(sender.yres)]
@@ -335,5 +362,6 @@ def rcontrib(*, sender, modifier: str, octree: Union[str, Path], out, opt) -> No
             out.mkdir(exist_ok=True)
             out = out / "%04d.hdr"
             cmd += ["-ffc", "-x", str(sender.xres), "-y", str(sender.yres)]
-        cmd += ["-o", out, "-M", modifier_path, str(octree)]
+        cmd += ["-o", str(out), "-M", modifier_path, str(octree)]
+        logger.info("Running rcontrib with:\n%s", " ".join(cmd))
         sp.run(cmd, check=True, input=stdin)
