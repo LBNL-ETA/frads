dd8cc81003794b4cf3f32a9a8881f1de74e56640
Taoning Wang <taoning@gmail.com>
2023-07-31 15:21:34 -0700

    test(test_sky.py): update

--- test/test_sky.py
+++ test/test_sky.py
@@ -20,30 +20,30 @@ epw_path = test_dir / "Resources" / "USA_CA_Oakland.Intl.AP.724930_TMY3.epw"
 wea_path = test_dir / "Resources" / "oak.wea"
 
 
-def test_basis_glow():
-    basis = "r1"
-    result = sky.basis_glow(basis)
-    assert result == ("#@rfluxmtx h=u\n\n"
-                      "void glow groundglow\n"
-                      "0\n0\n4 1 1 1 0\n\n"
-                      "groundglow source ground\n"
-                      "0\n0\n4 0 0 -1 180\n\n"
-                      "#@rfluxmtx u=+Y h=r1\n\n"
-                      "void glow skyglow\n"
-                      "0\n0\n4 1 1 1 0\n\n"
-                      "skyglow source sky\n"
-                      "0\n0\n4 0 0 1 180\n")
+# def test_basis_glow():
+#     basis = "r1"
+#     result = sky.basis_glow(basis)
+#     assert result == ("#@rfluxmtx h=u\n\n"
+#                       "void glow groundglow\n"
+#                       "0\n0\n4 1 1 1 0\n\n"
+#                       "groundglow source ground\n"
+#                       "0\n0\n4 0 0 -1 180\n\n"
+#                       "#@rfluxmtx u=+Y h=r1\n\n"
+#                       "void glow skyglow\n"
+#                       "0\n0\n4 1 1 1 0\n\n"
+#                       "skyglow source sky\n"
+#                       "0\n0\n4 0 0 1 180\n")
 
 
-def test_gen_sun_source_full():
-    """Generate sun sources for matrix generation."""
-    reinsrc4_path = test_dir / "Resources" / "reinsrc4.rad"
-    mf = 4
-    prim_str, mod_str = sky.gen_sun_source_full(mf)
-    prims = pr.parse_primitive(prim_str)
-    answer_prims = utils.unpack_primitives(reinsrc4_path)
-    for aprim, prim in zip(prims, answer_prims):
-        assert str(aprim) == str(prim)
+# def test_gen_sun_source_full():
+#     """Generate sun sources for matrix generation."""
+#     reinsrc4_path = test_dir / "Resources" / "reinsrc4.rad"
+#     mf = 4
+#     prim_str, mod_str = sky.gen_sun_source_full(mf)
+#     prims = pr.parse_primitive(prim_str)
+#     answer_prims = utils.unpack_primitives(reinsrc4_path)
+#     for aprim, prim in zip(prims, answer_prims):
+#         assert str(aprim) == str(prim)
 
 def test_gendaymtx():
     """Generate a psuedo reinhart 6 sun matrix file given lat, lon, etc..."""
@@ -59,9 +59,9 @@ def test_filter_wea():
     #     remove_zero=True, daylight_hours_only=True)
     pass
 
-def test_check_sun_above_horizon():
-    sky.check_sun_above_horizon()
-    pass
+# def test_check_sun_above_horizon():
+#     sky.check_sun_above_horizon()
+#     pass
 
 def test_gendaylit_cmd():
     """Get a gendaylit command as a list."""
@@ -104,27 +104,27 @@ def test_gen_sun_source_culled():
     # sun_mtx.unlink()
     pass
 
-def test_filter_data_by_direct_sun():
-    meta = WeaMetaData("Oakland", "USA", 37.72, 122.22, 120, 2)
-    data = [
-        WeaData(datetime(2000, 1, 1, 6, 30), 0, 0),
-        WeaData(datetime(2000, 1, 1, 7, 30), 71, 1),
-        WeaData(datetime(2000, 1, 1, 8, 30), 463,  41),
-        WeaData(datetime(2000, 1, 1, 13, 30), 717, 110),
-        WeaData(datetime(2000, 7, 1, 15, 30), 473,  95),
-        WeaData(datetime(2000, 7, 1, 16, 30), 244,  16),
-        WeaData(datetime(2000, 1, 1, 17, 30),   0,   0),
-    ]
-    wnormal = geom.Vector(0, 1, 0)
-    filtered = sky.filter_data_by_direct_sun(data, meta, window_normal=[wnormal])
-    answer = [
-        WeaData(datetime(2000, 1, 1,  7, 30),  71,   1),
-        WeaData(datetime(2000, 1, 1,  8, 30), 463,  41),
-        WeaData(datetime(2000, 1, 1, 13, 30), 717, 110),
-        WeaData(datetime(2000, 7, 1, 15, 30), 473,  95),
-    ]
-    for res, ans in zip(filtered, answer):
-        assert res == ans
+# def test_filter_data_by_direct_sun():
+#     meta = WeaMetaData("Oakland", "USA", 37.72, 122.22, 120, 2)
+#     data = [
+#         WeaData(datetime(2000, 1, 1, 6, 30), 0, 0),
+#         WeaData(datetime(2000, 1, 1, 7, 30), 71, 1),
+#         WeaData(datetime(2000, 1, 1, 8, 30), 463,  41),
+#         WeaData(datetime(2000, 1, 1, 13, 30), 717, 110),
+#         WeaData(datetime(2000, 7, 1, 15, 30), 473,  95),
+#         WeaData(datetime(2000, 7, 1, 16, 30), 244,  16),
+#         WeaData(datetime(2000, 1, 1, 17, 30),   0,   0),
+#     ]
+#     wnormal = geom.Vector(0, 1, 0)
+#     filtered = sky.filter_data_by_direct_sun(data, meta, window_normal=[wnormal])
+#     answer = [
+#         WeaData(datetime(2000, 1, 1,  7, 30),  71,   1),
+#         WeaData(datetime(2000, 1, 1,  8, 30), 463,  41),
+#         WeaData(datetime(2000, 1, 1, 13, 30), 717, 110),
+#         WeaData(datetime(2000, 7, 1, 15, 30), 473,  95),
+#     ]
+#     for res, ans in zip(filtered, answer):
+#         assert res == ans
 
 def test_parser_epw():
     with open(epw_path) as rdr:
