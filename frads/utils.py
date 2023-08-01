2e613121bcf97f0a2180c78d2729997793e7d4fd
Taoning Wang <taoning@gmail.com>
2023-07-31 17:44:08 -0700

    fix(utils.py): parser error

--- frads/utils.py
+++ frads/utils.py
@@ -191,7 +191,7 @@ def add_manikin(manikin_file: str, manikin_name: str, zone: dict, position: List
         Zone dictionary is modified in place.
     """
     zone_primitives = parse_primitive(zone['model']['scene']['bytes'].decode())
-    zone_polygons = [parse_polygon(p) for p in zone_primitives]
+    zone_polygons = [parse_polygon(p) for p in zone_primitives if p.ptype == 'polygon']
     xmin, xmax, ymin, ymax, zmin, _ = geom.get_polygon_limits(zone_polygons)
     target = np.array([xmin + (xmax - xmin) * position[0], ymin + (ymax - ymin) * position[1], zmin])
     with open(manikin_file) as f:
