547d6c6b43940b262391a8d30962bd5699f06f39
Tammie Yu <tammie.cy@gmail.com>
2023-08-10 13:09:42 -0700

    feat(test_window):add test for customized window gap

--- test/test_window.py
+++ test/test_window.py
@@ -39,29 +39,28 @@ def test_simple_glazingsystem():
     )
 
 
-# def test_glazingsystem():
-#     """
-#     Test the GlazingSystem class.
-#     Build a GlazingSystem object consisting of the following:
-#         - a single layer of clear glass
-#         - a single layer of shading product
-#         - a 0.0127 m thick gap between the two layer. The gap is filled with 90% argon and 10% air.
-#     """
-#     gs_ec60 = GlazingSystem()
-#     gs_ec60.add_glazing_layer(glass_path)
-#     gs_ec60.add_shading_layer(shading_path)
-#     gs_ec60.gaps = [((AIR, 0.1), (ARGON, 0.9), 0.0127)]
-#     gs_ec60.add_shading_layer(shading_path)
-#     gs_ec60.gaps = [((AIR, 0.1), (ARGON, 0.9), 0.0127)]
+def test_customized_gap():
+    """
+    Test the building of a customized gap.
+    A 0.03 m thick gap between the two glass layers. The gap is filled with 90% argon and 10% air.
+    """
+    gs = GlazingSystem()
+    gs.add_glazing_layer(glass_path)
+    gs.add_glazing_layer(glass_path)
+    gs.gaps = [((AIR, 0.1), (ARGON, 0.9), 0.03)]
 
-#     assert gs_ec60.layers[0].name == "CLEAR_3.DAT"
-#     assert gs_ec60.layers[1].name == "klems_aniso_high.xml"
-#     assert gs_ec60.gaps[0][0][0] == AIR
-#     assert gs_ec60.gaps[0][1][0] == ARGON
-#     assert gs_ec60.gaps[0][0][1] == 0.1
-#     assert gs_ec60.gaps[0][1][1] == 0.9
-#     assert gs_ec60.gaps[0][2] == 0.0127
-# assert gs_ec60._thickness ==
+    assert gs.gaps[0][0][0] == AIR
+    assert gs.gaps[0][0][1] == 0.1
+    assert gs.gaps[0][1][0] == ARGON
+    assert gs.gaps[0][1][1] == 0.9
+    assert gs.gaps[0][2] == 0.03
+    assert gs._thickness == sum(
+        [
+            gs.layers[0].thickness / 1e3,
+            gs.gaps[0][2],
+            gs.layers[1].thickness / 1e3,
+        ]
+    )
 
 
 def test_window():
